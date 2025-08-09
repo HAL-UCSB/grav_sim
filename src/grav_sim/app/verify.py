0ff81756e5d03092eeffd91a28808ce01a790ce0
Arthur Caetano <caetano@ucsb.edu>
2025-08-08 19:22:03 -0700

    uv grav_sim verify checking for hand_segments and rom_csv

--- src/grav_sim/app/verify.py
+++ src/grav_sim/app/verify.py
@@ -1,13 +1,14 @@
+import os
+import pathlib
 from io import BytesIO
 from zipfile import ZipFile
+from urllib import request
 
 import gdown
 import requests
 import streamlit as st
 
-import pathlib
-
-from grav_sim import settings
+from grav_sim import settings, Settings
 
 
 def _rm_rf(path: pathlib.Path):
@@ -23,6 +24,26 @@ def _rm_rf(path: pathlib.Path):
 
 def all_assets_exist():
     return settings.mano_assets.exists() and settings.ycb_aff_assets.exists()
+
+
+def resolve_rom_csv():
+    url = r'https://github.com/HAL-UCSB/grav_sim/blob/main/assets/eatonhand_rom.csv'
+    with request.urlopen(url) as response:
+        content = response.read()
+        with settings.rom_csv.open('w') as f:
+            f.write(content)
+            st.rerun()
+
+
+def resolve_hand_segments():
+    url = r'https://github.com/HAL-UCSB/grav_sim/blob/main/assets/hand_segments.zip'
+    with request.urlopen(url) as response:
+        zip_content = response.read()
+        with ZipFile(zip_content) as hand_segments_zip:
+            hand_segments_zip.extractall(settings.assets)
+            unzipped = settings.assets / hand_segments_zip.filelist[0].filename
+            settings.mano_assets = unzipped
+            st.rerun()
 
 
 def resolve_mano():
@@ -72,11 +93,21 @@ def resolve_ycb_aff():
         st.rerun()
 
 
-
+st.write(
+    f'you can set a custom a path to a custom .env on the environment variable `{Settings._ENV_VAR_GRASPR_DOT_ENV}`')
+st.write(f'`{Settings._ENV_VAR_GRASPR_DOT_ENV}={os.environ.get(Settings._ENV_VAR_GRASPR_DOT_ENV, None)}`')
 settings_path = pathlib.Path(settings.model_config['env_file'])
-st.markdown(f'Settings located at {settings_path.absolute()}')
-st.json(settings.model_dump_json())
+st.markdown(f'Using settings located at `{settings_path.absolute()}`')
+st.code(settings_path.read_text(), language='bash')
 settings.assets.mkdir(exist_ok=True)
+
+st.markdown(f'Hand Segments {"✅" if settings.rom_csv.exists() else "❌"}')
+if not settings.rom_csv.exists():
+    resolve_rom_csv()
+
+st.markdown(f'Hand Segments {"✅" if settings.hand_segments_assets.exists() else "❌"}')
+if not settings.hand_segments_assets.exists():
+    resolve_hand_segments()
 
 st.markdown(f'MANO {"✅" if settings.mano_assets.exists() else "❌"}')
 if not settings.mano_assets.exists():
